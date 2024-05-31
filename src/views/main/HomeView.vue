da67ae7adbc5a61701e62c40d684fe9802ad700d
Bui Tien Dung <dungbt2202.forwork@gmail.com>
2024-05-31 14:32:30 +0700

    new feat

--- src/views/main/HomeView.vue
+++ src/views/main/HomeView.vue
@@ -17,29 +17,31 @@ let onLoginClick = () => {
 }
 </script>
 <template>
-    <v-container fluid>
-        <v-row>
-            <v-col cols="12">
-                <div>
-                    <div v-if="currentUser">
-                        <label>
-                            Welcome, {{ currentUser }}!
-                        </label>
-                        <v-btn @click="onLogoutClick">
-                            Test Logout
-                        </v-btn>
-                    </div>
-                    <div v-else>
-                        <label>
-                            No user logged in!
-                        </label>
-                        <v-btn @click="onLoginClick">
-                            Test Login
-                        </v-btn>
-                    </div>
-                </div>
-            </v-col>
-
-        </v-row>
-    </v-container>
+    <div class="h-50">
+        <v-img cover class="mt-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSflJwN5U47ntffuz3KFosPyXQXSwuJEo_tZw&s">
+                <v-row>
+                    <v-col>
+                        <v-row>
+                            <v-col cols="12">
+                                <label class="text-h2 font-weight-bold">
+                                    Cảm thấy mất quá nhiều thời gian trong việc tìm thú cưng yêu thích?
+                                </label>
+                            </v-col>
+                            <v-col cols="12">
+                                <label class="text-h4 font-weight-light">
+                                    Cảm thấy mất quá nhiều thời gian trong việc tìm thú cưng yêu thích?
+                                </label>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-btn></v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                    <v-col>
+                        
+                    </v-col>
+                </v-row>
+            </v-img>  
+    </div>
+            
 </template>
